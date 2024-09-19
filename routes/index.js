3dce6a0467256cc22d5ef5d27b4cd8b4fcd56f2b
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-09-19 19:40:39 +0100

    create routes for POST requests to signup and login forms

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const router = express.Router();
 
+require('../config/passport');
+
 const indexController = require('../controllers/indexController');
 
-router.get('/sign-up', indexController.signup_post);
+// router.get('/signup-form', indexController.form_signup_get);
+
+router.post('/signup-form', indexController.form_signup_post);
+
+router.post('/login-form', [
+  body('username')
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage('Username must be specified'),
+  body('password')
+    .trim()
+    .isLength({ min: 6 }),
+], indexController.form_login_post);
 
 module.exports = router;
