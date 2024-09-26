aa3c51ea65ccf8f7aa5082750d04e49b9bbfe6e0
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-09-26 22:50:40 +0100

    update/clean route handlers

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,31 @@ const router = express.Router();
 require('../config/passport');
 
 const indexController = require('../controllers/indexController');
+const articleController = require('../controllers/articleController');
 
-// router.get('/signup-form', indexController.form_signup_get);
-
+// POST Signup Form
 router.post('/signup-form', indexController.form_signup_post);
 
+// POST Login Form
 router.post('/login-form', [
   body('username')
     .trim()
     .isLength({ min: 5 })
-    .withMessage('Username must be specified'),
+    .withMessage('Username too short'),
   body('password')
-    .trim()
-    .isLength({ min: 6 }),
+    .trim(),
+  // .isLength({ min: 6 }),
 ], indexController.form_login_post);
+
+// GET all Articles
+router.get('/articles', articleController.article_get);
+
+// PROTECTED ROUTES
+
+// GET all comments for specific article
+router.get('/articles/:id/comments', articleController.user_comment_post);
+
+// POST Article
+router.get('/test', articleController.article_post);
 
 module.exports = router;
