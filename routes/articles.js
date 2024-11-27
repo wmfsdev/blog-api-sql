bc675ace7dac330e1eb630ee4481b8bb861df5f7
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-11-27 19:03:07 +0000

    remove comments

--- routes/articles.js
+++ routes/articles.js
@@ -4,12 +4,6 @@ const router = express.Router();
 
 const articleController = require('../controllers/articleController');
 
-// router.get('/', articleController.article_get);
-
 router.get('/create', articleController.article_post);
-
-// it's the address that shouldn't contain descriptions i.e. GET/POST
-// but it is fine to include those "descriptions" in function names like
-// article_get
 
 module.exports = router;
