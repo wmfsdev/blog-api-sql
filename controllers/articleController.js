2c1c342c2c4b7eafee073c896c23d3722a6bf563
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-05-28 18:42:23 +0100

    add checks for user role in protected routes and remove comments

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 
 // GET article by ID
 exports.article_get = asyncHandler(async (req, res, next) => {
-  // NOT FROM CMS
   if (req.headers.origin !== process.env.CMS_URL) {
     const article = await prisma.article.findMany({
       where: {
@@ -59,7 +58,6 @@ exports.article_comments_get = asyncHandler(async (req, res, next) => {
 });
 
 exports.articles_get = asyncHandler(async (req, res, next) => {
-  // NOT from CMS - any origin
   if (req.headers.origin !== process.env.CMS_URL) {
     const articles = await prisma.article.findMany({
       orderBy: {
@@ -70,24 +68,19 @@ exports.articles_get = asyncHandler(async (req, res, next) => {
       },
     });
     return res.json(articles);
-  } // ELSE can only be CMS
-  console.log('pre auth post header check');
+  }
   passport.authenticate('jwt', async (err, user, info) => {
-    console.log('auth start');
     if (!user) {
-      console.log('no user');
       return res.status(401).json({ message: 'not authorised' });
     }
     const articles = await prisma.article.findMany({});
-    console.log('articles: ', articles);
     return res.json(articles);
-    // return res.status(200).json();
   })(req, res, next);
 });
 
 exports.article_delete = asyncHandler(async (req, res, next) => {
   passport.authenticate('jwt', async (err, user, info) => {
-    if (!user) {
+    if (!user || user.role !== 'ADMIN') {
       return res.status(401).json({ message: 'not authorised' });
     }
     const article = await prisma.article.delete({
@@ -95,7 +88,6 @@ exports.article_delete = asyncHandler(async (req, res, next) => {
         id: Number(req.params.id),
       },
     });
-    console.log('delete', article);
     return res.status(200).json(article);
   })(req, res, next);
 });
@@ -103,7 +95,7 @@ exports.article_delete = asyncHandler(async (req, res, next) => {
 // PUT Article
 exports.article_update = asyncHandler(async (req, res, next) => {
   passport.authenticate('jwt', async (err, user, info) => {
-    if (!user) {
+    if (!user || user.role !== 'ADMIN') {
       return res.status(401).json({ message: 'not authorised' });
     }
     const update = await prisma.article.update({
@@ -116,7 +108,6 @@ exports.article_update = asyncHandler(async (req, res, next) => {
         publish: req.body.publish,
       },
     });
-    console.log('pub status: ', update);
     return res.status(200).json(update);
   })(req, res, next);
 });
@@ -124,9 +115,10 @@ exports.article_update = asyncHandler(async (req, res, next) => {
 // POST Article
 exports.article_post = asyncHandler(async (req, res, next) => {
   passport.authenticate('jwt', async (err, user, info) => {
-    if (!user) {
+    if (!user || user.role !== 'ADMIN') {
       return res.status(401).json({ message: 'not authorised' });
     }
+
     const { title, body } = req.body;
     const { id } = user;
 
@@ -147,21 +139,15 @@ exports.user_comment_post = (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      console.log('validation errors');
       const err = new Error('validation failed');
       err.statusCode = 422;
       err.data = errors.array();
       throw err;
     }
     passport.authenticate('jwt', async (err, user, info) => {
-      console.log(user);
       if (!user) {
         return res.status(401).json({ message: 'not authorised' });
       }
-      // check against user details that user has permission to post as said user
-      // should be fine: user can only contain the ID of a non-malformed token i.e.
-      // the one you should be authenticating with. can't change the ID before, it
-      // will fail auth.
       const userId = user.id;
       const articleId = Number(req.params.id);
       const { comment } = req.body;
@@ -176,25 +162,18 @@ exports.user_comment_post = (req, res, next) => {
       return res.status(200).json(info);
     })(req, res, next);
   } catch (error) {
-    // err throw - sends array of errors to client
-    console.log('catch node err');
     const status = error.statusCode;
-    console.log(error.data);
     res.status(status).json(error.data);
   }
 };
 
 exports.user_comment_delete = asyncHandler(async (req, res, next) => {
-  console.log('user comment delete');
-
   passport.authenticate('jwt', async (err, user, info) => {
     if (!user) {
       return res.status(401).json({ message: 'not authorised' });
     }
-    // include user id as hidden input to compare - frontend solution
-    const userId = Number(user.id); // from token
-    // check that User has permission to delete the specific comment!
-    // look at payload of the token and compare User IDs
+
+    const userId = Number(user.id);
     const commentId = Number(req.body.comment);
 
     const deletionResult = await prisma.comment.deleteMany({
@@ -215,7 +194,6 @@ exports.user_comment_delete = asyncHandler(async (req, res, next) => {
     });
 
     if (deletionResult.count === 0) {
-      console.log('deletionResult', deletionResult);
       return res.status(400).json();
     }
     return res.status(200).json(info);
